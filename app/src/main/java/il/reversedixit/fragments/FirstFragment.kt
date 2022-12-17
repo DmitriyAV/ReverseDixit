929daf02a066ecf0c1bbf8ff4d8e6420ed85f885
DmitriyAV <dima.alekceev@gmail.com>
2022-12-17 16:15:25 +0300

    ReverseDixit

---
+++ app/src/main/java/il/reversedixit/fragments/FirstFragment.kt
@@ -0,0 +1,50 @@
+package il.reversedixit.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import il.reversedixit.R
+import il.reversedixit.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.description.text
+        binding.ok.setOnClickListener {
+
+            findNavController().navigate(
+                R.id.action_FirstFragment_to_SecondFragment,
+                bundleOf("description" to binding.description.text.toString())
+            )
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/il/reversedixit/viewmodels/GameViewModel.kt
@@ -0,0 +1,9 @@
+package il.reversedixit.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel() : ViewModel() {
+
+    val description = MutableLiveData("")
+}
